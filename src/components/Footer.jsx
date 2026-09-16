37ad2a1a8b54598eb258a69373079bb3a6d54860
Subash S <168352251+Subash5454@users.noreply.github.com>
2026-09-16 17:04:33 +0530

    Update Footer with LinkedIn URL

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,2 @@
 import React from 'react';
-export default function Footer(){ return <footer><p>© 2026</p></footer>; }
+export default function Footer(){ return <footer><p>© 2026 | <a href="https://linkedin.com/in/subash-s-4784002a2">LinkedIn</a></p></footer>; }
